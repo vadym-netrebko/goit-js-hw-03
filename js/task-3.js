797e36f755ba94c0bc6788d8ac18931d0ae4e302
Vadym Netrebko <pm.vadym@gmail.com>
2025-04-21 04:02:00 +0100

    done

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,15 @@
-function checkForSpam(message) {
-    message = message.toLowerCase();
-    if (message.includes('spam') || message.includes('sale')) {
-        return true;
-    } else {
-        return false;
+function filterArray(numbers, value) {
+let filteredNumbers = [];
+for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] > value) {
+        filteredNumbers.push(numbers[i]);
     }
 }
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+return filteredNumbers;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
